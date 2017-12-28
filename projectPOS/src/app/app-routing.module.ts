94f2d98ad405615473e0cbd69170d0b6881378d0
danthemango <danthemango@gmail.com>
2017-12-28 13:14:11 -0800

    Fixed routing modules and added components

---
+++ projectPOS/src/app/app-routing.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+
+import { AppComponent } from './app.component';
+import { PosComponent } from './pos/pos.component';
+import { InventoryComponent } from './inventory/inventory.component';
+import { ReportsComponent } from './reports/reports.component';
+import { FaqComponent } from './faq/faq.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PosComponent
+  },
+  {
+    path: 'inventory',
+    component: InventoryComponent
+  },
+  {
+      path: 'reports',
+      component: ReportsComponent
+  }
+  {
+      path: 'faq',
+      component: FaqComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- projectPOS/src/app/app.module.ts
+++ projectPOS/src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { PosComponent } from './pos/pos.component';
+import { InventoryComponent } from './inventory/inventory.component';
+import { ReportsComponent } from './reports/reports.component';
+import { FaqComponent } from './faq/faq.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PosComponent,
+    InventoryComponent,
+    ReportsComponent,
+    FaqComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
